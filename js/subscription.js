0ee5d04722c2f83beb385d39f8a454f1a6d7c831
Alexia E. Smith <washuu@gmail.com>
2016-04-19 15:45:09 -0500

    Wrap in MW. for potential fix.

--- js/subscription.js
+++ js/subscription.js
@@ -1,174 +1,176 @@
-$(document).ready(function() {
-	$(window).load(function() {
-		try {
-			var filterValues = JSON.parse($('#filtervalues').val());
-		} catch (SyntaxError) {
-			var filterValues = {};
-		}
-
-		if ($("#price").length) {
-			setupSlider(
-				'price',
-				(filterValues.default.price.min_price ? Math.floor(filterValues.default.price.min_price) : 0.0),
-				(filterValues.default.price.max_price ? Math.ceil(filterValues.default.price.max_price) : 0.0),
-				(filterValues.user.price.min_price ? Math.floor(filterValues.user.price.min_price) : 0.0),
-				(filterValues.user.price.max_price ? Math.ceil(filterValues.user.price.max_price) : 0.0)
-			);
-		}
-
-		$(".filter_bar form fieldset").append(createNamedFilter("providers", filterValues.default.providers, filterValues.user.providers).prepend($("<span>").text(mw.msg('sub_th_provider_id'))));
-
-		$(".filter_bar form fieldset").append(createNamedFilter("plans", filterValues.default.plans, filterValues.user.plans).prepend($("<span>").text(mw.msg('sub_th_plan_name'))));
-
-		if ($("input#begins_after").length) {
-			setupDatePicker($("input#begins_after"));
-		}
-
-		if ($("input#expires_before").length) {
-			setupDatePicker($("input#expires_before"));
-		}
-
-		$('input#min_date').change(function() {
-			var currentVal = $(this).val();
-			var time = new Date(0);
-			if (currentVal > 0) {
-				var existEpochDate = new Date((currentVal * 1000) + (time.getTimezoneOffset() * 60000));
-				var picker = '#begins_after';
-				$(picker).datepicker("setDate", existEpochDate);
+(function(mw, $) {
+	$(document).ready(function() {
+		$(window).load(function() {
+			try {
+				var filterValues = JSON.parse($('#filtervalues').val());
+			} catch (SyntaxError) {
+				var filterValues = {};
 			}
-		}).change();
 
-		$('input#max_date').change(function() {
-			var currentVal = $(this).val();
-			var time = new Date(0);
-			if (currentVal > 0) {
-				var existEpochDate = new Date((currentVal * 1000) + (time.getTimezoneOffset() * 60000));
-				var picker = '#expires_before';
-				$(picker).datepicker("setDate", existEpochDate);
+			if ($("#price").length) {
+				setupSlider(
+					'price',
+					(filterValues.default.price.min_price ? Math.floor(filterValues.default.price.min_price) : 0.0),
+					(filterValues.default.price.max_price ? Math.ceil(filterValues.default.price.max_price) : 0.0),
+					(filterValues.user.price.min_price ? Math.floor(filterValues.user.price.min_price) : 0.0),
+					(filterValues.user.price.max_price ? Math.ceil(filterValues.user.price.max_price) : 0.0)
+				);
 			}
-		}).change();
-	});
 
-	/**
-	 * Setup a date picker.
-	 *
-	 * @access	public
-	 * @param	object	Input element for the date picker.
-	 * @return	void
-	 */
-	function setupDatePicker(element) {
-		$(element).datepicker(
-			{
-				dateFormat: "yy-mm-dd",
-				constrainInput: true,
-				onSelect: function(dateText) {
+			$(".filter_bar form fieldset").append(createNamedFilter("providers", filterValues.default.providers, filterValues.user.providers).prepend($("<span>").text(mw.msg('sub_th_provider_id'))));
+
+			$(".filter_bar form fieldset").append(createNamedFilter("plans", filterValues.default.plans, filterValues.user.plans).prepend($("<span>").text(mw.msg('sub_th_plan_name'))));
+
+			if ($("input#begins_after").length) {
+				setupDatePicker($("input#begins_after"));
+			}
+
+			if ($("input#expires_before").length) {
+				setupDatePicker($("input#expires_before"));
+			}
+
+			$('input#min_date').change(function() {
+				var currentVal = $(this).val();
+				var time = new Date(0);
+				if (currentVal > 0) {
+					var existEpochDate = new Date((currentVal * 1000) + (time.getTimezoneOffset() * 60000));
+					var picker = '#begins_after';
+					$(picker).datepicker("setDate", existEpochDate);
+				}
+			}).change();
+
+			$('input#max_date').change(function() {
+				var currentVal = $(this).val();
+				var time = new Date(0);
+				if (currentVal > 0) {
+					var existEpochDate = new Date((currentVal * 1000) + (time.getTimezoneOffset() * 60000));
+					var picker = '#expires_before';
+					$(picker).datepicker("setDate", existEpochDate);
+				}
+			}).change();
+		});
+
+		/**
+		 * Setup a date picker.
+		 *
+		 * @access	public
+		 * @param	object	Input element for the date picker.
+		 * @return	void
+		 */
+		function setupDatePicker(element) {
+			$(element).datepicker(
+				{
+					dateFormat: "yy-mm-dd",
+					constrainInput: true,
+					onSelect: function(dateText) {
+						var dataHolder = '#'+$(this).attr('data-input');
+						var date = $(this).datepicker("getDate");
+						var offset = date.getTimezoneOffset() * 60;
+						var epoch = date.getTime() / 1000 - offset;
+
+						$(dataHolder).val(epoch);
+					}
+				}
+			).change(function() {
+				if (!$(this).val()) {
 					var dataHolder = '#'+$(this).attr('data-input');
-					var date = $(this).datepicker("getDate");
-					var offset = date.getTimezoneOffset() * 60;
-					var epoch = date.getTime() / 1000 - offset;
-
-					$(dataHolder).val(epoch);
+					$(dataHolder).val('');
 				}
-			}
-		).change(function() {
-			if (!$(this).val()) {
-				var dataHolder = '#'+$(this).attr('data-input');
-				$(dataHolder).val('');
-			}
-		}).keyup(function() {
-			$(this).change();
-		});
-	}
-
-	/**
-	 * Setup a slider filter.
-	 * http://codepen.io/ignaty/pen/EruAe
-	 *
-	 * @access	public
-	 * @param	string	Filter name, also the name of the ID for the main element.
-	 * @param	mixed	Integer or float of the absolute minimum allowed value.
-	 * @param	mixed	Integer or float of the absolute maximum allowed value.
-	 * @param	mixed	Integer or float of the user selected minimum allowed value.
-	 * @param	mixed	Integer or float of the user selected minimum allowed value.
-	 * @return	void
-	 */
-	function setupSlider(filterName, absoluteMin, absoluteMax, currentMin, currentMax) {
-		var element = $("#"+filterName);
-		$(element).slider({
-			range: true,
-			min: absoluteMin,
-			max: absoluteMax,
-			values: [currentMin, currentMax],
-			slide: function(event, ui) {
-				$('.ui-slider-handle:eq(0) .price-range-min', this).html('$' + ui.values[0]);
-				$('input[name="min_price"]', this).val(ui.values[0]);
-				$('.ui-slider-handle:eq(1) .price-range-max', this).html('$' + ui.values[1]);
-				$('input[name="max_price"]', this).val(ui.values[1]);
-				$('.price-range-both').html('$' + ui.values[0] + ' - $' + ui.values[1]);
-
-				if (slideCollision($('.price-range-min', this), $('.price-range-max', this)) == true) {
-					$('.price-range-min, .price-range-max', this).css('opacity', '0');
-					$('.price-range-both', this).css('display', 'block');
-				} else {
-					$('.price-range-min, .price-range-max', this).css('opacity', '1');
-					$('.price-range-both', this).css('display', 'none');
-				}
-			}
-		});
-
-		$('.ui-slider-range', $(element)).append('<span class="price-range-both value">$' + $(element).slider('values', 0) + ' - ' + $(element).slider('values', 1 ) + '</span>');
-
-		$('.ui-slider-handle:eq(0)', $(element)).append('<span class="price-range-min value">$' + $(element).slider('values', 0) + '</span>');
-		$('input[name="min_price"]', $(element)).val($(element).slider('values', 0));
-
-		$('.ui-slider-handle:eq(1)', $(element)).append('<span class="price-range-max value">$' + $(element).slider('values', 1) + '</span>');
-		$('input[name="max_price"]', $(element)).val($(element).slider('values', 1));
-	}
-
-	//http://codepen.io/ignaty/pen/EruAe
-	function slideCollision($div1, $div2) {
-		var x1 = $div1.offset().left;
-		var w1 = 40;
-		var r1 = x1 + w1;
-		var x2 = $div2.offset().left;
-		var w2 = 40;
-		var r2 = x2 + w2;
-
-		if (r1 < x2 || x1 > r2) {
-			return false;
+			}).keyup(function() {
+				$(this).change();
+			});
 		}
 
-		return true;
-	}
+		/**
+		 * Setup a slider filter.
+		 * http://codepen.io/ignaty/pen/EruAe
+		 *
+		 * @access	public
+		 * @param	string	Filter name, also the name of the ID for the main element.
+		 * @param	mixed	Integer or float of the absolute minimum allowed value.
+		 * @param	mixed	Integer or float of the absolute maximum allowed value.
+		 * @param	mixed	Integer or float of the user selected minimum allowed value.
+		 * @param	mixed	Integer or float of the user selected minimum allowed value.
+		 * @return	void
+		 */
+		function setupSlider(filterName, absoluteMin, absoluteMax, currentMin, currentMax) {
+			var element = $("#"+filterName);
+			$(element).slider({
+				range: true,
+				min: absoluteMin,
+				max: absoluteMax,
+				values: [currentMin, currentMax],
+				slide: function(event, ui) {
+					$('.ui-slider-handle:eq(0) .price-range-min', this).html('$' + ui.values[0]);
+					$('input[name="min_price"]', this).val(ui.values[0]);
+					$('.ui-slider-handle:eq(1) .price-range-max', this).html('$' + ui.values[1]);
+					$('input[name="max_price"]', this).val(ui.values[1]);
+					$('.price-range-both').html('$' + ui.values[0] + ' - $' + ui.values[1]);
 
-	function updateRangeDisplay(element, min, max) {
-		$(element).text(min+" - "+max);
-	}
+					if (slideCollision($('.price-range-min', this), $('.price-range-max', this)) == true) {
+						$('.price-range-min, .price-range-max', this).css('opacity', '0');
+						$('.price-range-both', this).css('display', 'block');
+					} else {
+						$('.price-range-min, .price-range-max', this).css('opacity', '1');
+						$('.price-range-both', this).css('display', 'none');
+					}
+				}
+			});
 
-	/**
-	 * Create a named filter set of check boxes.
-	 *
-	 * @access	public
-	 * @param	string	Filter Key/Name to use for form handling.
-	 * @param	object	List of default filters to include in the group filter.
-	 * @param	object	List of user filters to include in the group filter.
-	 * @return	object	Built HTML jQuery Object
-	 */
-	function createNamedFilter(filterKey, defaultFilters, userFilters) {
-		var container = $("<div>").addClass('named_filter').append("<ul>");
+			$('.ui-slider-range', $(element)).append('<span class="price-range-both value">$' + $(element).slider('values', 0) + ' - ' + $(element).slider('values', 1 ) + '</span>');
 
-		$.each(defaultFilters, function(unused, filter) {
-			var input = $("<input>").attr('type', 'checkbox').attr('name', filterKey+"[]").val(filter);
+			$('.ui-slider-handle:eq(0)', $(element)).append('<span class="price-range-min value">$' + $(element).slider('values', 0) + '</span>');
+			$('input[name="min_price"]', $(element)).val($(element).slider('values', 0));
 
-			if (userFilters && userFilters.length && userFilters.indexOf(filter) > -1) {
-				$(input).attr('checked', true);
+			$('.ui-slider-handle:eq(1)', $(element)).append('<span class="price-range-max value">$' + $(element).slider('values', 1) + '</span>');
+			$('input[name="max_price"]', $(element)).val($(element).slider('values', 1));
+		}
+
+		//http://codepen.io/ignaty/pen/EruAe
+		function slideCollision($div1, $div2) {
+			var x1 = $div1.offset().left;
+			var w1 = 40;
+			var r1 = x1 + w1;
+			var x2 = $div2.offset().left;
+			var w2 = 40;
+			var r2 = x2 + w2;
+
+			if (r1 < x2 || x1 > r2) {
+				return false;
 			}
 
-			var item = $("<li>").append($("<label>").append(input).append(filter));
+			return true;
+		}
 
-			$("ul", container).append(item);
-		});
+		function updateRangeDisplay(element, min, max) {
+			$(element).text(min+" - "+max);
+		}
 
-		return container;
-	}
-});
+		/**
+		 * Create a named filter set of check boxes.
+		 *
+		 * @access	public
+		 * @param	string	Filter Key/Name to use for form handling.
+		 * @param	object	List of default filters to include in the group filter.
+		 * @param	object	List of user filters to include in the group filter.
+		 * @return	object	Built HTML jQuery Object
+		 */
+		function createNamedFilter(filterKey, defaultFilters, userFilters) {
+			var container = $("<div>").addClass('named_filter').append("<ul>");
+
+			$.each(defaultFilters, function(unused, filter) {
+				var input = $("<input>").attr('type', 'checkbox').attr('name', filterKey+"[]").val(filter);
+
+				if (userFilters && userFilters.length && userFilters.indexOf(filter) > -1) {
+					$(input).attr('checked', true);
+				}
+
+				var item = $("<li>").append($("<label>").append(input).append(filter));
+
+				$("ul", container).append(item);
+			});
+
+			return container;
+		}
+	});
+}(mediaWiki, jQuery));
