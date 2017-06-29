cbbea8e658e3be6767e1810300cc80bdf9554800
Alexia E. Smith <washuu@gmail.com>
2017-06-29 16:37:13 -0500

    Missing alias files.

---
+++ Subscription.alias.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Curse Inc.
+ * Subscription
+ * Subscription Aliases
+ *
+ * @author 		Alexia E. Smith
+ * @copyright	(c) 2017 Curse Inc.
+ * @license		All Rights Reserved
+ * @package		Subscription
+ * @link		https://www.curse.com/
+ *
+**/
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Subscription' => ['Subscription']
+];
